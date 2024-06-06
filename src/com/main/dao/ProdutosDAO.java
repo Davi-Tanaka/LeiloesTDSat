17463b745e04dea8b2adf8c84c00ede6ccc5eeaf
Davi <davitanakaofc@gmail.com>
2024-06-06 17:03:01 -0400

    Implementação de metodos da classe ProdutosDAO.java

--- src/com/main/dao/ProdutosDAO.java
+++ src/com/main/dao/ProdutosDAO.java
@@ -1,45 +1,62 @@
 package com.main.dao;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import com.main.dto.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import com.main.global.Global;
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
-    }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
+    Connection conn = Global.retornarConexao();
 
+    public boolean cadastrarProduto (ProdutosDTO produto) throws SQLException  {
+        PreparedStatement prep = conn.prepareStatement("INSERT INTO produtos(nome, valor, status) VALUES (?,?,?)");
+
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());            
+
+        
+        return prep.executeUpdate() > 0;
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos() throws SQLException {
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        PreparedStatement prep = conn.prepareStatement("SELECT id, nome, valor, status FROM produtos");
+        ResultSet resultSet;
+        
+        resultSet = prep.executeQuery();
+        
+        while(resultSet.next()) {
+            int     id      = resultSet.getInt("id");
+            String  nome    = resultSet.getString("nome");
+            int     valor   = resultSet.getInt("valor");
+            String  status  = resultSet.getString("status");
+
+            ProdutosDTO prod = new ProdutosDTO();
+            
+            prod.setId(id);
+            prod.setNome(nome);
+            prod.setValor(valor);
+            prod.setStatus(status);
+
+            listagem.add(prod);
+        }
+
+        return listagem;
+    }       
+    
+    public boolean vender(int produtoId) {
+        try {
+            PreparedStatement prep = conn.prepareStatement("UPDATE produtos SET status='Vendido' WHERE id = ?");
+            prep.setInt(1, produtoId);
+            
+            return prep.executeUpdate() > 0;         
+        } catch(SQLException error) {
+            return false;
+        }        
+    }
+}
